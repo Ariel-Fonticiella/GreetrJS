beb15d9f839927469b66df576a96859ab34e8769
Ariel Fonticiella <ariel@fonticiella.io>
2018-02-19 14:25:34 -0500

    added some comments to explain what framework does

--- Greetr.js
+++ Greetr.js
@@ -1,64 +1,57 @@
 /*******************************************************************************
-  First, we create a new execution context for our entire Framework. All of the
-  variables that are declared there are safe. We're only exposing what we want on 
-  the Global Object. We do this by creating "Safe Code" by structuring an IIFE 
-  around the code that we wrote.
+  Greeter is a reusable library/framework that is used for, you guessed it, Greetings!
 
-  The first thing that happens is that the IIFE executes, creating a new Execution 
-  Context giving us the variables we need to run. And so now the whole greeter is 
-  safe inside of the IIFE and ready to be reused by anybody.
+  When given a first name, last name, and optional language, it will generate formal and informal greetings.
+
+  Greeter supports: English and Spanish languages, jQuery.
 *******************************************************************************/
 
-
+// First, we create a new execution context for our entire Framework.
+// All of the variables that are declared are safe. We're only exposing
+// on the Global Object what we want. We do this by creating "Safe Code" by
+// structuring an IIFE around the code that we wrote.
 
 (function(global, $){
+  // We pass two arguments which will be what we need access to. These
+  // arguments are the Global Variable 'global' and the jQuery variable '$'.
 
-  // instead of being a function constructor, to avoid having
-  // to always write the 'new' keyword, the function returns the results of a
-  // different funciton constructor.
-
-  var Greetr = function(firstName, lastName, language){
-
-      // 'return new Greetr.init' will use a function constructor to generate
-      // the object. That way you don't have to always setup the object with
-      // the 'new' keyword. 
+  let Greetr = function(firstName, lastName, language){
 
       return new Greetr.init(firstName, lastName, language);
+        // To avoid always writing the 'new' keyword, this function returns the // results of a different funciton constructor.
   };
 
-  // Greetr.init prototype points to this empty funciton.
-  // We add all our methods and properties here.
-
-  Greetr.prototype = {};
-
-  // This is the actual Constructor Funciton being returned.
+  //  Our Methods and Properties
+  //  are all added here.
+  //                  |
+  Greetr.prototype = { };
 
   Greetr.init = function(firstName, lastName, language) {
+      // This is the actual Constructor Funciton being returned.
 
-      // Create a variable named 'self' that points to 'this' so we know where it points later.
-
-      // now 'self' points to the empty object created by the 'new' operator.
-
-      var self = this;
-
-      // Function Constructor now builds an object and gives it three properties
-      // and sets its value if you pass something into the Funciton Constructor,
-      // otherwise sets some defaults.
+      let self = this;
+        // We create a variable named 'self' that points to the 'this' keyword
+        // so we know where it points later. Now 'self' points to the empty
+        // object that was created by the 'new' operator returned by Greetr.
 
       self.firstName = firstName || " ";
       self.lastName  = lastName  || " ";
       self.language  = language  || "en";
+        // The function constructor now builds an object and gives it three
+        // properties and sets their value's if you pass something into the
+        // funciton construct, otherwise it will set some defaults.
 
   };
 
-  // Any objects created with the Greetr.init function will point to
-  // Greetr.prototype for it's prototype chain.
-
   Greetr.init.prototype = Greetr.prototype;
-
-  // This will expose Greetr to the Global Object.
-  // So, on the Global Object, the two names 'G$' and 'Greetr' will point to the 'var Greetr = (){...}' value.
+    // Any objects created with the Greetr.init function will point to
+    // Greetr.prototype for it's prototype chain.
 
   global.Greetr = global.G$ = Greetr;
+    // This will expose Greetr to the Global Object, bc we pass in 'window' as
+    // the 'global' argument.
 
-}(window, jQuery));
+    // So, on the Global Object, the two names '.G$' and '.Greetr' will point to
+    // the 'var Greetr = function(){...}' value.
+
+}(window, jQuery)); // function(global, $)
