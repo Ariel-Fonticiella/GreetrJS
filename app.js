ff0db737381025f0d2c3bfb5363d67aae2bab884
Ariel Fonticiella <ariel@fonticiella.io>
2018-02-19 19:26:28 -0500

    added some final comments for clarification

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-// Whenever you use Greetr, you just have to say G$ then pass it three parameters (firstName, lastName, language). And what it gives you back is an Object.
-
+// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
 var g = G$('Ariel', 'Fonticiella');
-g.greet().setLang('es').greet(true);
+
+// use our chainable methods
+g.greet().setLang('en').greet(true).log();
+
+// let's us use our object on the 'click' of the login button
+$('#login').click(function() {
+
+    // create a new 'Greetr' obj (let's pretend we know the name from the login)
+    var loginGrtr = G$('Ariel', 'Fonticiella');
+
+    // hide the login on the screen
+    $('#logindiv').hide();
+
+    // fire off an HTML greeting, passing the '#greeting' as the selector and the chosen language, and log the welcom as well.
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
